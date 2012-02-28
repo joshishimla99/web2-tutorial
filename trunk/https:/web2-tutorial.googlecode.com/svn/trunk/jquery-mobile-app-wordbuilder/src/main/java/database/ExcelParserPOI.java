09c5fca07e3990355e9310c74d144bef0449f32b
deogaya@gmail.com <deogaya@gmail.com@6e8e7ef4-4208-8090-e5ed-98a1030fe770>
2012-02-28 10:45:59 +0000

    Committing the changes for running the migration and introducing spring transcation for better database management.

--- trunk/https:/web2-tutorial.googlecode.com/svn/trunk/jquery-mobile-app-wordbuilder/src/main/java/database/ExcelParserPOI.java
+++ trunk/https:/web2-tutorial.googlecode.com/svn/trunk/jquery-mobile-app-wordbuilder/src/main/java/database/ExcelParserPOI.java
@@ -2,56 +2,83 @@ package database;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
-
-import javax.naming.directory.DirContext;
+import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
 
 public class ExcelParserPOI {
-	public static void main(String[] args) {
-		XeroundDataAccess access = new XeroundDataAccess();
+	
+	private XeroundDAO xeroundDAO; 
+	
+	public XeroundDAO getXeroundDAO() {
+		return xeroundDAO;
+	}
+
+	public void setXeroundDAO(XeroundDAO dao) {
+		this.xeroundDAO = dao;
+	}
+
+	public void populateDatabase() {
+		System.out.println("Inside populate database");
 		File directory = new File("D:/Wordlist/");
 		String filename[] = directory.list();
 
 		for (int i = 0; i < 1; i++) {
-			HSSFWorkbook wb = null;
-			try {
-				// InputStream is = new FileInputStream(new
-				// File("/media/FreeAgent GoFlex Drive/start here/GRE/Wordlist/Excel/Wordlist/A Wordlist 1-322.xls"));
-				InputStream is = new FileInputStream(new File("D:/Wordlist/"
-						+ filename[i]));
-				POIFSFileSystem fs = new POIFSFileSystem(is);
-				wb = new HSSFWorkbook(fs);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			// start appending the values
-			StringBuffer sb = new StringBuffer();
-
-			HSSFSheet sheet = wb.getSheetAt(0);
-
-			// loop for every row in each worksheet
-			for (Iterator rows = sheet.rowIterator(); rows.hasNext();) {
-				HSSFRow row = (HSSFRow) rows.next();
-				String word = getRowColumnValue(row, 1);
-				String usage = getRowColumnValue(row, 2);
-				String meaning = getRowColumnValue(row, 3);
-				access.insert(word, usage, meaning);
-			}
+			System.out.println("Fetching excel data for " + filename[i]);
+			List<HSSFSheet> sheets = getWorkBookSheets(filename, i);
+			populateWorkbookData(sheets);
 		}
-		access.shutdown();
 	}
 
+	private void populateWorkbookData(List<HSSFSheet> sheets) {
+		for (HSSFSheet sheet : sheets) {
+			System.out.println("Populating the data for the sheet ");
+			// loop for every row in each worksheet
+			for (Iterator<Row> rows = sheet.rowIterator(); rows.hasNext();) {
+				HSSFRow row = (HSSFRow) rows.next();
+				if (row.getRowNum() != 0 ) {
+					System.out.println("Populating the data for the row " + row.getRowNum());
+					String word = getRowColumnValue(row, 1);
+					String usage = getRowColumnValue(row, 2);
+					String meaning = getRowColumnValue(row, 3);
+					xeroundDAO.insert(word, usage, meaning);					
+				}
+			}				
+		}
+	}
+
+	private List<HSSFSheet> getWorkBookSheets(String[] filename, int i) {
+		System.out.println("Retrieving all the sheet information for " + filename[i]);
+		HSSFWorkbook wb = null;
+		try {
+			// InputStream is = new FileInputStream(new
+			// File("/media/FreeAgent GoFlex Drive/start here/GRE/Wordlist/Excel/Wordlist/A Wordlist 1-322.xls"));
+			InputStream is = new FileInputStream(new File("D:/Wordlist/"
+					+ filename[i]));
+			POIFSFileSystem fs = new POIFSFileSystem(is);
+			wb = new HSSFWorkbook(fs);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		int noOfSheets = wb.getNumberOfSheets();
+		List<HSSFSheet> sheets = new ArrayList<HSSFSheet>();
+		for (int sheetIndex = 0 ; sheetIndex < noOfSheets ; sheetIndex++) {
+			sheets.add(wb.getSheetAt(sheetIndex));	
+		}
+		System.out.println("The number of sheets are " + sheets.size());
+		return sheets;
+	}
+	
 	private static String getRowColumnValue(HSSFRow row, int c) {
 		HSSFCell cell = row.getCell((int)c);
 		if (cell != null) {
