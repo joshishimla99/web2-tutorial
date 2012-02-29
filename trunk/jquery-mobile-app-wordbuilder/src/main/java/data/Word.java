e43bd3215088bd210bd685be29c64ac65b6bba53
deogaya@gmail.com <deogaya@gmail.com@6e8e7ef4-4208-8090-e5ed-98a1030fe770>
2012-02-29 00:44:55 +0000

    moving the code to the actual location

---
+++ trunk/jquery-mobile-app-wordbuilder/src/main/java/data/Word.java
@@ -0,0 +1,32 @@
+package data;
+
+public class Word {
+	private String word;
+	private String meaning;
+	private String usage;
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+
+	public String getUsage() {
+		return usage;
+	}
+
+	public void setUsage(String usage) {
+		this.usage = usage;
+	}
+
+}
