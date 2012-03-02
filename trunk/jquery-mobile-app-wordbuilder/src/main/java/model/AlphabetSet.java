680ffff9a71c6602eda47ca87e4b1dde403324a2
deogaya@gmail.com <deogaya@gmail.com@6e8e7ef4-4208-8090-e5ed-98a1030fe770>
2012-03-02 03:37:49 +0000

    webserver and service changes

---
+++ trunk/jquery-mobile-app-wordbuilder/src/main/java/model/AlphabetSet.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlphabetSet {
+
+        private List<Character> characters;
+
+        AlphabetSet() {
+                characters = new ArrayList<Character>();
+                int a = 65;
+                for (int i = 0; i < 26; i++) {
+                        characters.add((char) (a + i));
+                }
+
+        }
+
+        public List<Character> getCharacters() {
+                return characters;
+        }
+
+        public void setCharacters(List<Character> characters) {
+                this.characters = characters;
+        }
+
+        public Character getChar(int index) {
+                return characters.get(index);
+        }
+
+}
---
+++ trunk/jquery-mobile-app-wordbuilder/src/main/java/service/WordsHandler.java
@@ -0,0 +1,12 @@
+package service;
+
+import java.util.List;
+
+import model.Sentence;
+import model.Word;
+
+
+public interface WordsHandler {
+        public List<Word> getRandomWords();
+        public void storeSentences(Sentence sentence);
+}
